b0c485134dbea3ba7cdcf7b9e214d38bbed95826
storm1486 <patelmit8292@gmail.com>
2023-05-08 17:29:36 -0400

    Implement searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -19,7 +19,13 @@ export interface Game {
 const useGames =(
   gameQuery: GameQuery
 ) => useData<Game>('/games', 
-{params: {genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder}}, [gameQuery])
+{params: {
+  genres: gameQuery.genre?.id, 
+  platforms: gameQuery.platform?.id, 
+  ordering: gameQuery.sortOrder, 
+  search: gameQuery.searchText
+}}, 
+  [gameQuery])
 
 
 export default useGames;

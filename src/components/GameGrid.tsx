01542f9b48ce4afe1309f54f5c1bee53955199dd
storm1486 <patelmit8292@gmail.com>
2023-05-08 13:28:26 -0400

    format

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,16 +3,22 @@ import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 
 const GameGrid = () => {
-    const {games, error} = useGames();
+  const { games, error } = useGames();
 
-    return (
-      <>
-        {error && <Text> {error}</Text>}
-        <SimpleGrid columns ={{sm: 1, md: 2, lg:3, xl: 5}} padding= {10} spacing = {10}>
-          {games.map((game) => (<GameCard key = {game.id} game = {game}/>))}
-        </SimpleGrid>
-      </>
-    );
+  return (
+    <>
+      {error && <Text> {error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding={10}
+        spacing={10}
+      >
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </SimpleGrid>
+    </>
+  );
 };
 
 export default GameGrid;

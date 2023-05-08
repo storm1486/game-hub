45ac48f9fe75d496f7afb6e6f15ae691a035d552
storm1486 <patelmit8292@gmail.com>
2023-05-08 18:08:46 -0400

    Customize the theme to get different colors

--- src/theme.ts
+++ src/theme.ts
@@ -5,6 +5,21 @@ const config: ThemeConfig ={
     initialColorMode: 'dark'
 };
 
-const theme = extendTheme({config});
+const theme = extendTheme({
+    config,
+    colors: {
+        gray: {
+            50: '#ffe3ff',
+            100: '#ffb3f1',
+            200: '#fe81e6',
+            300: '#fc4fda',
+            400: '#fa20cf',
+            500: '#e10ab6',
+            600: '#b0038e',
+            700: '#7e0065',
+            800: '#4d003e',
+        }
+    }
+});
 
 export default theme;
